6d96548f95874ba9b09b09eb95fe1c1220d17056
Aneesh Nelavelly <35740650+Aneeshn25@users.noreply.github.com>
2019-07-05 11:06:51 -0400

    open port 80 to security group project12_sg

--- project12.tf
+++ project12.tf
@@ -122,6 +122,12 @@ resource "aws_security_group" "project12_sg" {
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
  egress {
     from_port   = 0
     to_port     = 0
